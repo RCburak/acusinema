eeb3465fc55d860f232b8964a9dd89c9431a20c6
RCburak <kaya.burak.ibrahim42@gmail.com>
2025-10-11 01:39:29 +0300

    admin paneli

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,35 @@
-from django.contrib import admin
+# core/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Acusinema, Event
+
+# ... (AcusinemaAdmin sınıfınız burada aynı kalıyor) ...
+class AcusinemaAdmin(admin.ModelAdmin):
+    list_display = ('title', 'director', 'release_date')
+    search_fields = ('title', 'director')
+    list_filter = ('release_date', 'director')
+    date_hierarchy = 'release_date'
+
+
+# --- GÜNCELLENEN EVENT ADMIN SINIFI ---
+class EventAdmin(admin.ModelAdmin):
+    # Liste sayfasında slug'ı da gösterelim
+    list_display = ('title', 'location', 'event_date', 'slug')
+    list_filter = ('event_date', 'location')
+    search_fields = ('title', 'location')
+
+    # Ekleme/Düzenleme sayfasındaki alanları grupluyoruz
+    fieldsets = (
+        ('Etkinlik Detayları', {
+            'fields': ('title', 'slug', 'description')
+        }),
+        ('Zaman ve Mekan', {
+            'fields': ('event_date', 'location')
+        }),
+    )
+
+    # 'slug' alanını 'title' alanından otomatik olarak doldur
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Acusinema, AcusinemaAdmin)
+admin.site.register(Event, EventAdmin)
